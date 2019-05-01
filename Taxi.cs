c2ecac41408c74fff96d6d84b56245510d97d8c0
UncleBob302 <iqbalgavi46@gmail.com>
2019-05-01 19:08:06 +0700

    member class project

---
+++ Taxi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Taxi
+    {
+        // properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("DriverName; {0}", DriverName);
+            Console.WriteLine("OnDuty; {0}", OnDuty);
+            Console.WriteLine("NumPassenger; {0}", NumPassenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("Jono sedang menjemput penumpang");
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("Jono selesai mengantar penumpang");
+        }
+
+
+    }
+}
